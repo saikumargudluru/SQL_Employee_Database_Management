fa506519f1792c3bf7b796da4ba68b058f7c2e91
saikumar gudluru <143861779+saikumargudluru@users.noreply.github.com>
2023-10-26 12:52:34 +0530

    Update employee database management project.sql

--- employee database management project.sql
+++ employee database management project.sql
@@ -1,110 +1,94 @@
---create database
-create database capstone_1;
-use capstone_1;
+--created database
+create database employee_database;
+use employee_database;
 
---create table
+--created  employee table
 create table employee(employeeid int,employeename varchar(10),gender char(1),deptno int,managerid int,salary int);
+
+--created  department table
 create table department(deptno int,deptname varchar(15),city varchar(10));
 
-drop table employee;
-drop table department;
-
-
+--added email column to employee table
 alter table employee
 add email varchar(50);
 
-
+--changed name city to location in department table
 exec sp_rename 'department.city','location';
 
-
+--changed  not null of column employeeid 
 alter table employee
 alter column employeeid int not null;
 
+--added primary key to  employeeid in employee table
 alter table employee
 add constraint pk_id primary key(employeeid);
 
-
+--applied  not null constraint  to column deptno
 alter table department
 alter column deptno int not null;
 
+--added primary key to  deptno in department table
 alter table department
 add constraint pk_depart_deptno primary key(deptno);
 
-
+--added foreign key to deptno in employee table
 alter table employee
 add constraint fk_employee_deptno foreign key(deptno)
 references department(deptno);
 
-alter table employee
-drop constraint fk_employee_deptno;
-
+--added foreign key to managerid in employee table
 alter table employee
 add constraint fk_employee_managerid foreign key(managerid)
 references employee(employeeid);
 
-
-alter table employee
-drop constraint fk_employee_managerid;
-
+--applied default constraint  to column email
 alter table employee
 add constraint default_employee_email default 'na'  for email;
 
-
+--applied  check constraint  to column location
 alter table department
 add constraint check_department_city check(location in ('noida','delhi','lucknow'));
 
-
+--applied  check constraint  to column salary
 alter table employee
 add constraint check_employee_salary check(salary >=0);
 
-
+-- insert data into department
 insert into department
 values(10,'SALES','noida'),(20,'IT','noida'),(30,'FINANCE','lucknow'),(40,'MARKETING','delhi');
 
+-- insert data into employee
 insert into employee(employeeid,employeename,gender,deptno,managerid,salary)
 values(1,'LINDA','F',10,null,75000),(2,'MARY','F',10,1,40000),(3,'ANJELA','F',10,2,32000),
 (4,'JASON','M',20,1,50000),(5,'AHMED','M',20,4,30000),(6,'MARIA','F',20,4,40000),(7,'JOHN','M',30,1,60000),
 (8,'JACK','M',30,7,55000),(9,'JUNE','F',30,7,40000),(10,'OLA','M',30,8,35000);
 
-
-select * from employee
-where left(employeename,1)=right(employeename,1);
-
-
-select * from employee
-where len(employeename) >(select len(employeename) from employee where employeeid=5);
-
-
-select string_agg(employeename,',')as name from employee;
-
-
-select CONCAT(left(employeename,1),right(employeename,1))as nickname from employee;
-
-
+ --find number_of_employees who belong to lucknow location
 select  count(e.employeeid) as number_of_employees  from employee e
 inner join
 department d
 on e.deptno=d.deptno and location='lucknow';
 
-
+--find deptno wise total salaries
 select  sum(salary) as total_salaried from employee
 group by deptno;
 
-
+--find deptno wise find min and max salaries
 select min(salary) as least_salaried ,max(salary) as maximum_salaried from employee
 group by deptno;
 
-
+--find gender wise salaries
 select gender,sum(salary) as total_salary from employee
 group by gender;
 
-
+--find number_of_employees who are belong to marketing and who having highest salary in the  departement
 select count(employeeid)as number_of_employees from employee 
 where salary > 
 (select avg(salary) from employee
 where deptno in (select deptno from department
 where deptname ='marketing'))
 
+ --find  employee details such  deptno,employeeid who are belong to finance  departement 
 select d.deptno,e.employeeid from employee e
 inner join
 department d
@@ -120,26 +104,7 @@ on e.deptno=d.deptno
 select count(e.employeeid) as number_of_employees from employee e
  (select employeeid from employee where deptno in (select deptno from department ) > (select employeeid from employee where deptno in (select deptno from department where deptname='finance'))
 
-
-
-
-
-
-
---F
-
---getdate()-returns the current date and time.
---datepart()-returns a specific part such as year,month,day,hour,minute and so on from  a date or time.
---datename()-returns the name of a specific date part from date or time.
---dateadd()-we can add or subtract specified interval such as year,month,day,hour,minute and so on from a date or time.
---datediff()-calcualtes the difference between two dates in a specified filed such as year,month,day.
---eomonth()-returns last day of month for the specified date.
---day()-returns the day from date.
---month()-returns the month from date.
---year()-returns the year from date.
---isdate()-it is used to validate a date value.
-
-
+--created sale table
 create table Sales(sid int not null,sales money,sdate date,eid int);
 alter table Sales 
 add constraint pk_Sales_sid primary key(sid);
@@ -154,7 +119,7 @@ values(101,4000,'2005-10-10',1),(102,2300,'2006-01-12',1),(103,9000,'2005-06-20'
 (121,2100,'2012-03-15',10),(122,9300,'2012-09-15',10),(123,9200,'2013-07-15',10),(124,8300,'2013-03-15',10);
 
 
-
+-- using dense rank i find top two salaries  and also get complete  employee details using subquery
 select * from 
 (select d.deptname, employeename,e.salary ,DENSE_RANK() over(order by salary desc)rnk from employee e
 inner join
@@ -162,7 +127,7 @@ department d
 on e.deptno=d.deptno and d.deptname='IT')t1
 where rnk<=2;
 
-
+-- using dense rank i find top two salaries over departement wise  and also get complete  employee details using subquery
 select * from
 (select d.deptname,e.employeename,e.salary,d.deptno,dense_rank() over (partition by d.deptno order by e.salary desc)rnk   from employee e
 inner join
@@ -171,7 +136,7 @@ on e.deptno=d.deptno
 )t1
 where rnk<=2;
 
-
+-- department wise total sales
 select d.deptname,sum(s.sales) total_sales  from  Sales s
 inner join
 employee e
@@ -182,20 +147,20 @@ on e.deptno=d.deptno
 group by d.deptname;
 
 
-
+--on october month who much sales happened  and their names
 select e.employeename,s.sales from employee e
 inner join
 Sales s
 on e.employeeid=s.eid and DATENAME(month,sdate)='october';
 
-
+--in table  not specific employee name then become top level as  manager name using coalese
 select  e.employeename as employee_name,coalesce(m.employeename,'TOP level')as manager_name  from employee e
 left join
 employee m
 on e.managerid=m.employeeid;
 
 
-
+--find null in eid in employee table
 select e.employeeid, e.employeename from employee e
 left join 
 sales s
@@ -204,18 +169,14 @@ where s.eid is null;
 
 
 
-
+ --find top 1 sales using dense rank and also get information eid and sales using subquery 
 select eid  from
 (select eid, sum(sales)as sales ,dense_rank() over( order by sum(sales) desc)rnk 
 from Sales group by eid )t1
  where rnk=1;
 
 
-
-
-
-
-
+--storted  both tables in view with encryption security
 create view vname_details
 with schemabinding,encryption
 as
@@ -226,10 +187,3 @@ on
 e.deptno=d.deptno;
 select * from vname_details;
 
-alter view vname_details
-as
-select e.employeename,d.deptname,e.salary,d.location from dbo.employee e
-inner join
-dbo.department d
-on
-e.deptno=d.deptno;
